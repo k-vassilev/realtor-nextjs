7660f42e59a16b07a6eb0b91df825ffc9285bbf5
Kristian Vassilev <kvasilev@devrix.com>
2021-12-07 15:20:19 +0200

    Added properties get service and render

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import Property from "../components/Property"
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -17,12 +18,8 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 		</Box>
 	</Flex>
 )
-	
-
-
 
 export default function Home({ propertiesForSale, propertiesForRent}) {
-	console.log(propertiesForSale, propertiesForRent);
   return (
     <Box>
 	  <Banner 
@@ -36,7 +33,7 @@ export default function Home({ propertiesForSale, propertiesForRent}) {
 		imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
 	  />
 	  <Flex flexWrap="wrap">
-		{/* Fetch properties for rentals and map over them*/}
+		{propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
 	  </Flex>
 	  <Banner 
 	  	purpose="Buy A HOME"
@@ -48,7 +45,7 @@ export default function Home({ propertiesForSale, propertiesForRent}) {
 		linkName="/search?purpose=for-sale"
 		imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
 	  />
-	  {/* Fetch properties for sales and map over them*/}
+	  {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Box>
   )
 }
