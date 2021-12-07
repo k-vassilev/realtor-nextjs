6ddddec6b25a15fc957b177098dc8ea087b0be17
Kristian Vassilev <kvasilev@devrix.com>
2021-12-07 14:54:13 +0200

    Created utils and added headers to RapidAPI

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchAPI = async (url) => {
+	const { data } = await axios.get((url), {
+		headers: {
+			'x-rapidapi-host': 'bayut.p.rapidapi.com',
+			'x-rapidapi-key': '89e3f57894mshee8cb6e4ae32696p1d96f7jsn92f88d8ea032'
+		  }
+	})
+}
